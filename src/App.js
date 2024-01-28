ef1168adcefaa91c621e02ea42854d9fa3497fa5
Priyanka <priyalalwani94@gmail.com>
2024-01-28 14:48:07 -0500

    commit1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Home Page Little Lemon</h1>
+     <h1>Little Lemon Restaurant</h1>
     </div>
   );
 }
