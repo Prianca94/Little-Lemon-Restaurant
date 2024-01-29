5cacf5b7d0c2b99e71dfbdfbeb1a752e98b07a80
Priyanka <priyalalwani94@gmail.com>
2024-01-29 00:20:03 -0500

    updatenav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Little Lemon Restaurant</h1>
-    </div>
+    <>
+     {/* <h1>Little Lemon Restaurant</h1> */}
+    <BrowserRouter>
+    <Nav/>
+     <Header/>
+    </BrowserRouter>
+    
+    </>
   );
 }
 
